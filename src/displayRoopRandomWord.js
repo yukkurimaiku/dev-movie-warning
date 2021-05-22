4e39ed6c720126ebb062d07a9b8155a9a18e9883
suberumaiku <suberumaiku@gmail.com>
2021-05-23 02:41:50 +0900

    表示速度変更

--- src/displayRoopRandomWord.js
+++ src/displayRoopRandomWord.js
@@ -1,5 +1,7 @@
 const createRamdomNumber = require('./createRamdomNumber');
 
+const roopTime = 300;
+
 /**
  * isplayWordsの中身を500ミリ秒ごとにランダムに表示
  * @param {Array[string]} displayRoopRandomWord 
@@ -9,7 +11,7 @@ const displayRoopRandomWord = (displayWords) => {
         const randomNumber = createRamdomNumber(displayWords.length - 1);
 
         console.log(`\u001b[32mYKMIK@DEVMOVIE:~$ ${displayWords[randomNumber]}`);
-    }, 500);
+    }, roopTime);
 };
 
 module.exports = displayRoopRandomWord;
